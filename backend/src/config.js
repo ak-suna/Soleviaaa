8b520b562b4613a0251c719dd216d3c90fd382c4
Anuska G.C. <165690567+ak-suna@users.noreply.github.com>
2026-05-30 02:41:52 +0545

    .

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    FRONTEND_URL: "https://soleviaaa-ebdj.vercel.app/",
+    FRONTEND_URL: "https://soleviaaa-ebdj.vercel.app",
     // BACKEND_URL: "http://localhost:5000",
 };
 
